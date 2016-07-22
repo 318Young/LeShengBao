1f788c25030ad276673a0feeaa70c7e050c9d8ef
OnceDaily <649846150@qq.com>
2016-07-22 10:15:36 +0800

    添加账户

--- app/src/main/java/com/young/leshengbao/MainActivity.java
+++ app/src/main/java/com/young/leshengbao/MainActivity.java
@@ -28,38 +28,41 @@ public class  MainActivity extends AppCompatActivity implements View.OnClickList
     private View headView;
     private CircleImageView iv_header;
     private TextView tv_name;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        try {
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
-        final ActionBar ab = getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-        ab.setDisplayHomeAsUpEnabled(true);
+            final ActionBar ab = getSupportActionBar();
+            ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+            ab.setDisplayHomeAsUpEnabled(true);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-        if (navigationView != null) {
-            setupDrawerContent(navigationView);
+            mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+            navigationView = (NavigationView) findViewById(R.id.nav_view);
+            if (navigationView != null) {
+                setupDrawerContent(navigationView);
+            }
+            headView = navigationView.getHeaderView(0);
+            iv_header = (CircleImageView) headView.findViewById(R.id.iv_user_header);
+            tv_name = (TextView) headView.findViewById(R.id.tv_name);
+
+
+            ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+            if (viewPager != null) {
+                setupViewPager(viewPager);
+            }
+            TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+            tabLayout.setupWithViewPager(viewPager);
+
+
+            iv_header.setOnClickListener(this);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        headView = navigationView.getHeaderView(0);
-        iv_header = (CircleImageView) headView.findViewById(R.id.iv_user_header);
-        tv_name = (TextView) headView.findViewById(R.id.tv_name);
-
-
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        if (viewPager != null) {
-            setupViewPager(viewPager);
-        }
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
-
-
-        iv_header.setOnClickListener(this);
     }
 
     @Override
